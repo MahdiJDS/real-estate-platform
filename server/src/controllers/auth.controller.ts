6643d752881caa6cd483a1191b646b798cbb8f21
MahdiJDS <mahdijahed56@gmail.com>
2026-06-08 08:17:49 +0330

    feat: create logic for controll auth route

---
+++ server/src/controllers/auth.controller.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from "express";
+import { signAccessToken, signRefreshToken } from "../utils/jwt";
+const bcrypt = require('bcrypt')
+import {prisma} from "../config/prisma";
+
+export const register = async (req: Request, res: Response) => {
+    try {
+        const { firstName, lastName, email, password } = req.body;
+
+        const existingUser = await prisma.user.findUnique({
+            where: { email },
+        });
+
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await prisma.user.create({
+            data: {
+                firstName,
+                lastName,
+                email,
+                password: hashedPassword,
+            },
+        });
+
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(500).json({ message: "Server Error" });
+    }
+};
+
+
+
+export const login = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await prisma.user.findUnique({ where: { email } });
+
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const isValid = await bcrypt.compare(password, user.password);
+
+        if (!isValid) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const accessToken = signAccessToken(user.id);
+        const refreshToken = signRefreshToken(user.id);
+
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: false, 
+        });
+
+        res.json({ accessToken, user });
+    } catch (err) {
+        res.status(500).json({ message: "Server Error" });
+    }
+};
