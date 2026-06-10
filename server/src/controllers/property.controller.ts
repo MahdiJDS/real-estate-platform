f9e30f51f8ecf4ae7ea06498c0f24aca07c8ce8b
MahdiJDS <mahdijahed56@gmail.com>
2026-06-10 05:42:43 +0330

    feat: Create updateProperty function to controll properties routes(update property data)

--- server/src/controllers/property.controller.ts
+++ server/src/controllers/property.controller.ts
@@ -114,3 +114,30 @@ export const getPropertyById = async (req: Request, res: Response) => {
         });
     }
 };
+
+
+// PATCH /api/properties/:id
+export const updateProperty = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const userId = req.user?.userId;
+
+        const property = await prisma.property.findUnique({ where: { id: Number(id) } });
+
+        if (!property) return res.status(404).json({ message: "Property not found" });
+
+        if (property.ownerId !== userId) return res.status(403).json({ message: "Forbidden" });
+
+        const updatedProperty = await prisma.property.update({
+            where: { id: Number(id) },
+            data: req.body,
+        });
+
+        res.json(updatedProperty);
+    } catch (error) {
+        res.status(500).json({
+            message: "Error updating property",
+            error: error instanceof Error ? error.message : error
+        });
+    }
+};
