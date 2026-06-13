3f88e4718de509d3944cfb3081346e7d432934b2
MahdiJDS <mahdijahed56@gmail.com>
2026-06-13 09:02:45 +0330

    feat: creatw new middleware to mangment image uploader

---
+++ server/src/middlewares/upload.middleware.ts
@@ -0,0 +1,13 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: "./uploads",
+
+    filename: (_req, file, cb) => {
+        cb(null, Date.now() + "-" + file.originalname);
+    },
+});
+
+export const uploadSingle = multer({
+    storage,
+});
