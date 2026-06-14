21242e276e6c74ff23419d0c12474f35b7492e4f
MahdiJDS <mahdijahed56@gmail.com>
2026-06-14 07:49:47 +0330

    fix: Fix and correct the import path

--- server/src/utils/uploadToCloudinary.ts
+++ server/src/utils/uploadToCloudinary.ts
@@ -1,4 +1,5 @@
-import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
+import { UploadApiResponse } from "cloudinary";
+import cloudinary from "../config/cloudinary"
 import { Readable } from "stream";
 
 export const uploadToCloudinary = (
