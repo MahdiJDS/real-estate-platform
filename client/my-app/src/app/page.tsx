efc3b2fd94925711319d913abb71cd6bde189730
MahdiJDS <mahdijahed56@gmail.com>
2026-06-22 01:56:38 +0330

    chore: Update this component to the real, industrial version to react query hydration and use searchParams to cash&fetch and search

--- client/my-app/src/app/page.tsx
+++ client/my-app/src/app/page.tsx
@@ -1,17 +1,27 @@
-import Hero from "@/components/home/hero";
-import SearchBar from "@/components/home/search-bar";
-import FeaturedProperties from "@/components/home/featured-properties";
+import { getProperties } from "@/services/properties.service";
+import HomeClient from "@/components/home/home-client";
+import { PropertyFilters } from "@/types/propertyfilters";
+import { dehydrate, QueryClient } from "@tanstack/react-query";
 
-export default function HomePage() {
-  return (
-    <div className="space-y-16">
+export default async function HomePage({
+  searchParams,
+}: {
+  searchParams: Promise<Record<string, string | undefined>>;
+}) {
+  const params = await searchParams;
+  const queryClient = new QueryClient();
 
-      <Hero />
+  const filters = {
+    city: params.city,
+    minPrice: params.minPrice ? Number(params.minPrice) : undefined,
+    maxPrice: params.maxPrice ? Number(params.maxPrice) : undefined,
+    sort: params.sort as PropertyFilters["sort"],
+  };
 
-      <SearchBar />
+  await queryClient.prefetchQuery({
+    queryKey: ["properties", filters],
+    queryFn: () => getProperties(filters),
+  });
 
-      <FeaturedProperties />
-
-    </div>
-  );
+  return <HomeClient dehydratedState={dehydrate(queryClient)} initialFilters={filters} />;
 }
