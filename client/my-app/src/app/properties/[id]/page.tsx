d7aca302677db34cbb49711b96f4676c66bce2b3
MahdiJDS <mahdijahed56@gmail.com>
2026-06-24 03:41:15 +0330

    feat(property): implement property details page with gallery, sidebar and details

--- client/my-app/src/app/properties/[id]/page.tsx
+++ client/my-app/src/app/properties/[id]/page.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import PropertyGallery from "@/components/property/property-gallery";
+import PropertyDetails from "@/components/property/property-details";
+import PropertySidebar from "@/components/property/property-sidebar";
 
-export default function page() {
-  return (
-    <div>page</div>
-  )
+import { getPropertyById } from "@/services/property.service";
+
+interface Props {
+    params: Promise<{
+        id: string;
+    }>;
+}
+
+export default async function PropertyPage({
+    params,
+}: Props) {
+    const { id } = await params;
+
+    const property = await getPropertyById(id);
+
+    return (
+        <section className="container mx-auto py-10">
+
+            <div className="space-y-8">
+
+
+
+                <div className="grid lg:grid-cols-[2fr_400px] items-center gap-10">
+
+
+                    <PropertyGallery
+                        imageUrl={property.imageUrl}
+                    />
+                    <PropertySidebar
+                        property={property}
+                    />
+
+                </div>
+                <PropertyDetails
+                    property={property}
+                />
+
+            </div>
+
+        </section>
+    );
 }
