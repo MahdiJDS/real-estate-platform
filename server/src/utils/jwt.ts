f6d43d81659b574015e408e69d02fc8e278108b4
MahdiJDS <mahdijahed56@gmail.com>
2026-07-03 09:12:35 +0330

    feat: implement access and refresh token verification

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -2,6 +2,9 @@ const jwt = require('jsonwebtoken')
 
 const ACCESS_SECRET = process.env.JWT_ACCESS_SECRET!;
 const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET!;
+export interface JwtPayload {
+  userId: number;
+}
 
 // Access Token
 export const signAccessToken = (userId: number) => {
@@ -12,3 +15,11 @@ export const signAccessToken = (userId: number) => {
 export const signRefreshToken = (userId: number) => {
   return jwt.sign({ userId }, REFRESH_SECRET, { expiresIn: "7d" });
 };
+
+export function verifyAccessToken(token: string | undefined): JwtPayload {
+  return jwt.verify(token, ACCESS_SECRET) as JwtPayload;
+}
+
+export function verifyRefreshToken(token: string): JwtPayload {
+  return jwt.verify(token, REFRESH_SECRET) as JwtPayload;
+}
