c7e1124e2140a9b28a2f26c14d358f2fd7cddece
MahdiJDS <mahdijahed56@gmail.com>
2026-07-03 09:30:01 +0330

    feat(auth): implement login endpoint

---
+++ server/src/controllers/auth/login.controller.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import { signAccessToken, signRefreshToken, verifyRefreshToken } from "../../utils/jwt";
+const bcrypt = require('bcrypt')
+import { prisma } from "../../config/prisma";
+
+export const login = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await prisma.user.findUnique({ where: { email } });
+
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const isValid = await bcrypt.compare(password, user.password);
+
+        if (!isValid) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const accessToken = signAccessToken(user.id);
+        const refreshToken = signRefreshToken(user.id);
+
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: false,
+        });
+
+        res.json({ accessToken, user });
+    } catch (error) {
+        console.log("LOGIN ERROR:", error);
+        res.status(500).json({
+            message: "Server Error",
+            error: error instanceof Error ? error.message : error
+        });
+    }
+};
