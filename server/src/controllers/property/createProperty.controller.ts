2881adc17102ab0a8643969a5d79f11dff067bb2
MahdiJDS <mahdijahed56@gmail.com>
2026-07-04 09:09:31 +0330

    refactor: reorganize project structure for better scalability and maintainability for property department

---
+++ server/src/controllers/property/createProperty.controller.ts
@@ -0,0 +1,65 @@
+import { prisma } from "../../config/prisma";
+import { Request, Response } from "express";
+import { uploadToCloudinary } from "../../utils/uploadToCloudinary";
+
+export const createProperty = async (
+    req: Request,
+    res: Response
+) => {
+    try {
+        const {
+            title,
+            description,
+            price,
+            city,
+            address,
+            area,
+            bedrooms,
+            bathrooms,
+        } = req.body;
+
+        const userId = req.user?.userId;
+
+        if (!userId) {
+            return res.status(401).json({
+                message: "Unauthorized",
+            });
+        }
+
+        let imageUrl: string | undefined;
+
+        if (req.file) {
+            const uploadResult = await uploadToCloudinary(req.file.buffer);
+            imageUrl = uploadResult.secure_url;
+        }
+
+
+        const property = await prisma.property.create({
+            data: {
+                title,
+                description,
+                price: Number(price),
+                city,
+                address,
+                area: Number(area),
+                bedrooms: Number(bedrooms),
+                bathrooms: Number(bathrooms),
+                ...(imageUrl && {
+                    imageUrl: imageUrl,
+                }),
+                ownerId: userId,
+            },
+            include: {
+                owner: true,
+            },
+        });
+
+        res.status(201).json(property);
+    } catch (error) {
+        res.status(500).json({
+            message: "Error creating property",
+            error: error instanceof Error ? error.message : error,
+        });
+    }
+};
+
