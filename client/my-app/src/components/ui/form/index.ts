5963b3220c3e006b66e998fc86b28342bbd41ecd
MahdiJDS <mahdijahed56@gmail.com>
2026-07-06 00:50:40 +0330

    feat(forms): add new barral exports for form components (form-password)

--- client/my-app/src/components/ui/form/index.ts
+++ client/my-app/src/components/ui/form/index.ts
@@ -2,3 +2,5 @@ export * from "./form-input";
 export * from "./form-number";
 export * from "./form-textare";
 export * from "./form-file";
+export * from "./form-password";
+export * from "./submit-button";
