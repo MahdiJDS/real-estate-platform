d2a8902b6500b55a7e2eb76dc7c797f3134bfc61
MahdiJDS <mahdijahed56@gmail.com>
2026-07-05 08:19:08 +0330

    feat(auth): add refresh, logout, and current user endpoints

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,9 +1,16 @@
 import express from "express";
-import { register, login } from "../controllers/auth.controller";
+import { register} from "../controllers/auth/register.controller";
+import { login } from "../controllers/auth/login.controller";
+import { refreshToken } from "../controllers/auth/refresh.controller";
+import { logout } from "../controllers/auth/logout.controller";
+import { me } from "../controllers/auth/me.controller";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/refresh", refreshToken);
+router.post("/logout", logout);
+router.post("/me", me);
 
 export default router;
