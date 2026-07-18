ae46026cf228589e4d4916796a6fd72f8074a9ac
MahdiJDS <mahdijahed56@gmail.com>
2026-07-18 10:10:56 +0330

    chore(config): add centralized CORS configuration

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,15 +4,11 @@ require('dotenv').config();
 const cookieParser = require('cookie-parser')
 import authRoutes from "./routes/auth.routes";
 import propertyRoutes from "./routes/property.routes";
+import corsOptions from "./config/cors";
 
 const app = express();
 
-app.use(
-    cors({
-        origin: "http://localhost:3000",
-        credentials: true,
-    })
-);
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
