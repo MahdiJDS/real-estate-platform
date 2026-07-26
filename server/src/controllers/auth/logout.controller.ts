47854c983311d433eab230f5d2708968e5286d7b
MahdiJDS <mahdijahed56@gmail.com>
2026-07-26 09:31:09 +0330

    refactor(auth): use shared cookie config for user logout

--- server/src/controllers/auth/logout.controller.ts
+++ server/src/controllers/auth/logout.controller.ts
@@ -1,14 +1,12 @@
 import { Request, Response } from "express";
+import { refreshCookieOptions } from "../../config/cookie";
 
 export const logout = async (
     _req: Request,
     res: Response
 ) => {
 
-    res.clearCookie("refreshToken", {
-        httpOnly: true,
-        sameSite: "lax",
-    });
+    res.clearCookie("refreshToken", refreshCookieOptions);
 
     return res.status(200).json({
         message: "Logged out successfully",
